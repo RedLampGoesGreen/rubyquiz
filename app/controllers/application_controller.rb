febd7de73fbe8859a2ce1c87334f0e36e42eea2f
fabian <fabian-kunzke@web.de>
2015-05-24 13:28:18 +0200

    fixed errors with devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotUnique, with: :show_errors
+  rescue_from SQLite3::ConstraintException, with: :show_errors
   
   #forces user to login first!
   before_action :authenticate_user!
--- app/models/install.rb
+++ app/models/install.rb
@@ -3,4 +3,5 @@ class Install < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :email, :id, presence: true, uniqueness: true
 end
