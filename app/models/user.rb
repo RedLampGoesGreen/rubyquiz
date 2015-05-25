39ac88f38081d7fb5a3907c7d9eaaad73b8cddf3
fabian <fabian-kunzke@web.de>
2015-05-25 09:59:33 +0200

    fixed an sign up bug where a id was asked for

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  validates :email, :user_id, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   has_one :player
 end
